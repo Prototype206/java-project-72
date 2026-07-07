46b4762efc90ac67a46b50a72a7a01a3ee1c8924
Prototype206 <ulyanow.mischa2017@yandex.ru>
2026-07-07 14:26:02 +0400

    create project

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+
+plugins {
+    id("java")
+    id("checkstyle")
+    id("jacoco")
+    id("org.sonarqube") version "7.3.1.8318"
+    application
+    id("io.github.goooler.shadow") version "8.1.8"
+    id("io.freefair.lombok") version "8.6"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
+}
+
+checkstyle {
+    toolVersion = "12.1.2"
+    configFile = file("config/checkstyle/checkstyle.xml")
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "Prototype206_java-project-72")
+    property("sonar.organization", "prototypes-organization")
+  }
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("app")
+    archiveClassifier.set("")
+    archiveVersion.set("")
+}
