89867252139d9b85103291de86b7c3b2aad04c59
Prototype206 <ulyanow.mischa2017@yandex.ru>
2026-07-09 12:59:56 +0400

    empty commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
 
     implementation("io.javalin:javalin-rendering:6.1.3")
     implementation("gg.jte:jte:3.1.12")
-    
     implementation("com.konghq:unirest-java:3.14.5")
     implementation("org.jsoup:jsoup:1.17.2")
 
